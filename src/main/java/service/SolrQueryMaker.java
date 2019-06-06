3cbd1c333dc6a6f093c7883cddcf551c1595b209
jonathan <jonathan.boss@uni-bamberg.de>
2019-06-06 15:51:32 +0200

    small bug fix

--- src/main/java/service/SolrQueryMaker.java
+++ src/main/java/service/SolrQueryMaker.java
@@ -78,7 +78,7 @@ public class SolrQueryMaker {
 	
 	public void setSearchFilterForComplexProperty(int idOfSolrObject, int parentfk, String schema) {
 
-		query.addFilterQuery("resourcetype_filter:\"nobjects\n" + "|||\n" + "N-Object###default\"");
+		setQuerySearchTerm("resourcetype_filter:\"nobjects\n" + "|||\n" + "N-Object###default\"");
 		String parentFKWithId = (parentfk+"-"+idOfSolrObject);
 		query.addFilterQuery("search.parentfk:"+parentFKWithId);
 		query.addFilterQuery("search.schema_s:"+schema);
