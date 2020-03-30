f36a824a0123145975066aa09b61bb20066510f0
Gantner, Florian Klaus <florian.gantner@uni-bamberg.de>
2020-03-30 06:44:37 +0000

    Feature additions

--- src/main/java/entitys/Researcher.java
+++ src/main/java/entitys/Researcher.java
@@ -19,6 +19,7 @@ import org.apache.olingo.commons.api.edm.provider.annotation.CsdlExpression;
 
 
 
+
 public class Researcher implements EntityModel {
 	
 	public final static String NAMESPACE = "dspace";
