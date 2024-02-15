0e48ef33d8c8f87078ffd1e9f75193517b5c924e
Gantner, Florian Klaus <florian.gantner@uni-bamberg.de>
2024-02-15 14:18:54 +0100

    replace deprecated solrclient

--- src/main/java/data/SolrConnector.java
+++ src/main/java/data/SolrConnector.java
@@ -3,7 +3,7 @@ package data;
 import java.io.IOException;
 
 import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.impl.Http2SolrClient;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
 import service.SolrQueryMaker;
@@ -15,7 +15,7 @@ public class SolrConnector {
 	
 	public void connectToDSpaceSolr() {
 		String urlString = System.getenv("SOLR_URL");
-		solr = new HttpSolrClient.Builder(urlString).build();
+		solr = new Http2SolrClient.Builder(urlString).build();
 	}
 	
 	public SolrDocumentList getData(SolrQueryMaker queryMaker) throws IOException {
